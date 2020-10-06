47119c169a8a5c1ed707ef067bb7a172d342f337
Joana Haase <joana.haase@tum.de>
2020-10-06 20:23:19 +0200

    new goals added

--- include/turtlebot2i_lfe_navigation/lfe_nav_config.h
+++ include/turtlebot2i_lfe_navigation/lfe_nav_config.h
@@ -56,18 +56,18 @@ namespace lfe_navigation {
         LfeNavConfig() {
 
             //navigation
-            navigation.goal1_pos_x = -0.07; //kraftraum: 0.5 //allraumtest: 0.33  //rtabmaphome: 2.0 //lfe: -17.3
-            navigation.goal1_pos_y = -3.08; //kraftraum:-2.3 //allraumtest: -1.75 //rthabmaphome: -3.3 //lfe: -26.2
+            navigation.goal1_pos_x = -5.95; //kraftraum: 0.5 //allraumtest: 0.33  //rtabmaphome: 2.0 //lfe: -17.3
+            navigation.goal1_pos_y = -12.1; //kraftraum:-2.3 //allraumtest: -1.75 //rthabmaphome: -3.3 //lfe: -26.2
             navigation.goal1_pos_z = 0.0;
             navigation.goal1_orientation = 0.0;
 
-            navigation.goal2_pos_x = -18.1; //kraftraum:1.56 //allraumtest: -2.4 //rtabmaphome: 0.4 //lfe: 1.13
-            navigation.goal2_pos_y = -26.7; //kraftraum:2.6 //allraumtest: -3.77 //rtabmaphome: -0.95 //lfe: -0.59
+            navigation.goal2_pos_x = 3.74; //kraftraum:1.56 //allraumtest: -2.4 //rtabmaphome: 0.4 //lfe: 1.13
+            navigation.goal2_pos_y = 4.83; //kraftraum:2.6 //allraumtest: -3.77 //rtabmaphome: -0.95 //lfe: -0.59
             navigation.goal2_pos_z = 0.0;
             navigation.goal2_orientation = 0.0;
 
-            navigation.goal3_pos_x = -5.57; //rtabmaphome: 1.5 //lfe:
-            navigation.goal3_pos_y = 6.9; //rtabmaphome: -1.0  //lfe:
+            navigation.goal3_pos_x = -0.27; //rtabmaphome: 1.5 //lfe:
+            navigation.goal3_pos_y = -1.85; //rtabmaphome: -1.0  //lfe:
             navigation.goal3_pos_z = 0.0;
             navigation.goal3_orientation = 0.0;
 
